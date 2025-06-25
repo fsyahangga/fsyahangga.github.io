7642210aaf36c662f73024c4ad4c120253c980f0
Angga Firmansyah <firmansyah.angga@gmail.com>
2025-06-25 15:49:28 +0700

    Update main.js

--- main.js
+++ main.js
@@ -113,19 +113,20 @@ function initProjectReveal() {
 }
 function initSectionShow() {
   const sections = document.querySelectorAll(".section");
-  
-      const observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-          if (entry.isIntersecting) {
-            entry.target.classList.add("show");
-          }
-        });
-      }, {
-        threshold: 0.1
-      });
-  
-      sections.forEach(section => {
-        observer.observe(section);
-      });
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+        observer.unobserve(entry.target); // opsional: hanya animasi sekali
+      }
     });
+  }, {
+    threshold: 0.1
+  });
+
+  sections.forEach(section => {
+    observer.observe(section);
+  });
 }
+
