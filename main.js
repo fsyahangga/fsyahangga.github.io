d2fdd4498418ad33db57ff915b3abb4007b10d3a
Angga Firmansyah <firmansyah.angga@gmail.com>
2025-06-25 15:44:51 +0700

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
   initSidebarToggle();
   initSmoothScroll();
   initScrollSpy();
+  initSectionShow();
   initSectionReveal(); // ⬅️ tambahkan ini
   initProjectReveal();
 });
@@ -109,4 +110,22 @@ function initProjectReveal() {
   }, { threshold: 0.1 });
 
   cards.forEach(card => observer.observe(card));
+}
+function initSectionShow() {
+  const sections = document.querySelectorAll(".section");
+  
+      const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+          }
+        });
+      }, {
+        threshold: 0.1
+      });
+  
+      sections.forEach(section => {
+        observer.observe(section);
+      });
+    });
 }
